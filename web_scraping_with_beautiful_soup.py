4aa5a5d330f878a3217ff0bdcee0a0e8eb3657c8
Shivam Mishra <mshivam019@gmail.com>
2021-11-26 14:45:42 +0530

    Add files via upload

---
+++ web_scraping_with_beautiful_soup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""Web Scraping with Beautiful Soup.ipynb
+
+### Step 1: Import Libraries
+"""
+#pip install beautifulsoup4
+#pip install pandas
+#pip install requests
+#pip install openpyxl
+from bs4 import BeautifulSoup # For extracting the data from the HTML page
+import pandas as pd # Data Manipulation and export
+from requests import get # Request URL to access content
+
+"""### Step 2: Create a variable url which contains the link"""
+
+print("Enter any year:")
+year = input()
+url = 'http://www.imdb.com/search/title?release_date='+year+'&sort=num_votes,desc&page=1'
+response = get(url)
+
+""" ### Step 3: Print the response"""
+
+print(response.text[:500])
+
+""" ### Step 3: Use Beautiful Soup to parse the data"""
+
+html_soup = BeautifulSoup(response.text, 'html.parser')
+
+movie_containers = html_soup.find_all('div', class_ = 'lister-item mode-advanced')
+
+"""### Step 4: Initiliaze the lists"""
+
+names = []
+imdb_ratings = []
+votes = []
+
+"""### Step 5: Start storing the data"""
+
+for countainer in movie_containers:
+    name = countainer.h3.a.text
+    names.append(name)
+    imdb_rating = float(countainer.strong.text)
+    imdb_ratings.append(imdb_rating)
+    vote = int(countainer.find('span', attrs = {'name':'nv'})['data-value'])
+    votes.append(vote)
+
+"""### Step 6: Verify the stored data"""
+
+print(names)
+print(imdb_ratings)
+print(votes)
+"""### Step 7: Store the data"""
+
+df = pd.DataFrame({'Name':names, 'Rating':imdb_ratings, 'Vote':votes})
+df.to_excel('Watchlist of '+year+'.xlsx', index=False, encoding='utf-8')
+
+"""### Step 8: Check the final output"""
+
+df.head()
+
